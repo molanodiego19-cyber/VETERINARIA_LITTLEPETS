dde5a27da3e9e0a6f0817bff40ca5939356219fc
molanodiego19-cyber <molanodiego19@gmail.com>
2026-05-27 10:39:30 -0500

    Fix requirements

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -105,7 +105,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+        os.environ.get("ballast.proxy.rlwy.net:44227")
     )
 }
 
