f79d100e2d8f7726434922cdacb5f05a75283b23
molanodiego19-cyber <molanodiego19@gmail.com>
2026-05-27 10:35:28 -0500

    Fix requirements

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -101,6 +101,13 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
