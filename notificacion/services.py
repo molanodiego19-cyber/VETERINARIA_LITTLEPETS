0290c5863c565635321461c5d86a529f8b984a01
jhonnyamontes <jhonny_montes@soy.sena.edu.co>
2026-05-29 15:34:54 -0500

    services modificado temproalmente

--- notificacion/services.py
+++ notificacion/services.py
@@ -2,14 +2,52 @@ from .models import Notificacion, PlantillaNotificacion
 from django.template import Template, Context
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
+import traceback
+
 
 def render_template(texto, contexto):
     template = Template(texto)
     return template.render(Context(contexto))
 
 
-from django.conf import settings
-import traceback
+def crear_notificacion(usuario, plantilla_nombre, cita=None, contexto=None):
+
+    if contexto is None:
+        contexto = {}
+
+    try:
+        plantilla = PlantillaNotificacion.objects.get(
+            nombre=plantilla_nombre,
+            activo=True
+        )
+
+    except PlantillaNotificacion.DoesNotExist:
+        print(f"❌ No existe plantilla: {plantilla_nombre}")
+        return None
+
+    asunto = render_template(
+        plantilla.asunto_plantilla,
+        contexto
+    )
+
+    cuerpo = render_template(
+        plantilla.cuerpo_plantilla,
+        contexto
+    )
+
+    notificacion = Notificacion.objects.create(
+        usuario=usuario,
+        plantilla=plantilla,
+        cita=cita,
+        canal=plantilla.canal,
+        asunto=asunto,
+        cuerpo_mensaje=cuerpo
+    )
+
+    print("📩 Notificación pendiente")
+
+    return notificacion
+
 
 def enviar_email(notificacion):
 
@@ -35,12 +73,17 @@ def enviar_email(notificacion):
             to=[notificacion.usuario.correo]
         )
 
-        email.attach_alternative(html_template, "text/html")
+        email.attach_alternative(
+            html_template,
+            "text/html"
+        )
 
         print("📤 Intentando enviar correo...")
+
         email.send()
 
         notificacion.marcar_enviada()
+
         print("✅ Email enviado")
 
     except Exception as e:
