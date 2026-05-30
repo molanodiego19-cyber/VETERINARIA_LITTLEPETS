6125bb91c7a71771e47bb69eca0f6c661232e6a1
molanodiego19-cyber <molanodiego19@gmail.com>
2026-05-29 20:10:54 -0500

    actalizar a brevo correo

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -143,11 +143,13 @@ print("EMAIL_HOST_PASSWORD ENV:", os.getenv("EMAIL_HOST_PASSWORD"))
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = 'littlepetscolombia@gmail.com'
 
 EMAIL_TIMEOUT = 10
