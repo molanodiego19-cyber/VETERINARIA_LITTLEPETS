e953c06223a927443a48231d74662c6e27e23f4c
molanodiego19-cyber <molanodiego19@gmail.com>
2026-05-29 20:15:48 -0500

    cambio de SMTP a SSL

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -144,7 +144,7 @@ print("EMAIL_HOST_PASSWORD ENV:", os.getenv("EMAIL_HOST_PASSWORD"))
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
