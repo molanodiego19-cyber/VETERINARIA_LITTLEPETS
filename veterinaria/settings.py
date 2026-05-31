d45e5709f8c145d6e6bfd98c705c52819b52a743
molanodiego19-cyber <molanodiego19@gmail.com>
2026-05-30 19:30:08 -0500

    requirements listo

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -19,7 +19,21 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
 ]
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
+            ],
+        },
+    },
+]
 # =====================
 # APPS
 # =====================
@@ -80,7 +94,6 @@ else:
             "PORT": "3306",
         }
     }
-
 # =====================
 # STATIC
 # =====================
